f4a3f99904a34b351724298d9ea9b832b80eaada
iinsue <hinsu93@naver.com>
2021-12-30 23:42:36 +0900

    chapter4 Connection 관리를 분리

--- DbApp.java
+++ DbApp.java
@@ -1,15 +1,26 @@
+import java.sql.Connection;
 import java.util.Scanner;
 
 import madang.Madang;
+import madang.ConnManager;
 
 public class DbApp {
     
     private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
+        Connection c = ConnManager.getConnection();
 
-        Madang book = new Madang();
+        Madang book = new Madang(c);
+
+        book.select();
         System.out.print("책 검색 > ");
         String keyword = scanner.nextLine();
         book.select(keyword);
+
+        try {
+            c.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
